e46f1ac4038ab3366fffd8ca5fe6b99bbe636346
Paul Mullins <paulmullins@protonmail.com>
2021-10-09 22:40:43 -0400

    The vega-react graph works

--- react/src/App.js
+++ react/src/App.js
@@ -1,3 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { VegaLite } from 'react-vega'
+
 import logo from './logo.svg';
 import './App.css';
 //import { ElasticConnect } from './components/ElasticConnect';
@@ -38,23 +42,34 @@ function App() {
     console.log( (await response.json()));
   }
   testBackend()
+  const spec = {
+    width: 400,
+    height: 200,
+    mark: 'bar',
+    encoding: {
+      x: { field: 'a', type: 'ordinal' },
+      y: { field: 'b', type: 'quantitative' },
+    },
+    data: { name: 'table' }, // note: vega-lite data attribute is a plain object instead of an array
+  }
+  
+  const barData = {
+    table: [
+      { a: 'A', b: 28 },
+      { a: 'B', b: 55 },
+      { a: 'C', b: 43 },
+      { a: 'D', b: 91 },
+      { a: 'E', b: 81 },
+      { a: 'F', b: 53 },
+      { a: 'G', b: 19 },
+      { a: 'H', b: 87 },
+      { a: 'I', b: 52 },
+    ],
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <VegaLite spec={spec} data={barData} />,
     </div>
   );
 }
