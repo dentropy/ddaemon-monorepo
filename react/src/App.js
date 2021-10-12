2b8f7c3ef9f6fca8065ef53b10e1c143d4b04b42
Paul Mullins <paulmullins@protonmail.com>
2021-10-12 19:20:54 -0400

    Now using react context API

--- react/src/App.js
+++ react/src/App.js
@@ -8,21 +8,31 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import { Sidebar } from './components/Sidebar';
+import { Context } from './components/Context';
 function App() {
-
+  const [context, setContext] = useState();
+  useEffect(() => {
+    setContext({
+      "most":"message",
+      "per" :"team"
+    })
+  }, [])
   return (
     <div className="App">
+      <Context.Provider value={[context, setContext]}>
       <Box sx={{ width: 1 }}>
           <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
               <Box gridColumn="span 2">
-                <SelectFromList />
                 <Sidebar />
+                {JSON.stringify(context)}
               </Box>
               <Box gridColumn="span 10">
+                <button onClick={() => {console.log(context)}}>print context</button>
                 <RenderBasicGraph />
               </Box>
           </Box>
       </Box>
+      </Context.Provider>
     </div>
   );
 }
--- react/src/components/Sidebar.js
+++ react/src/components/Sidebar.js
@@ -1,52 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import React, { useState, useEffect, useContext } from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import { Context } from './Context';
+// messages|edits|deletes|reactions|URL's|reactions sent
 
 export const Sidebar =  () => {
+  const [context, setContext] = useContext(Context);
+  console.log(context)
   return (
-    <div>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Accordion 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion disabled>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography>Disabled Accordion</Typography>
-        </AccordionSummary>
-      </Accordion>
-    </div>
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Most _____</FormLabel>
+      <RadioGroup
+        aria-label="most_blank"
+        defaultValue="messages"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel value="messages" control={<Radio />} label="messages" 
+          onClick={() => {var tmpContext = context; tmpContext.most = "messages"; setContext(tmpContext)}} />
+        <FormControlLabel value="edits" control={<Radio />} label="edits" 
+          onClick={() => {var tmpContext = context; tmpContext.most = "edits"; console.log(tmpContext);setContext(tmpContext)}}  />
+        <FormControlLabel value="deletes" control={<Radio />} label="deletes" 
+          onClick={() => {var tmpContext = context; tmpContext.most = "deletes"; console.log(tmpContext);setContext(tmpContext)}} />
+        <FormControlLabel value="URLs" control={<Radio />} label="URLs's" 
+          onClick={() => {var tmpContext = context; tmpContext.most = "URLs"; console.log(tmpContext);setContext(tmpContext)}} />
+        <FormControlLabel value="reactions_sent" control={<Radio />} label="reactions sent"
+          onClick={() => {var tmpContext = context; tmpContext.most = "reactions_sent"; console.log(tmpContext);setContext(tmpContext)}}  />
+      </RadioGroup>
+      <FormLabel component="legend">per _____</FormLabel>
+      <RadioGroup
+        aria-label="per <Blank>"
+        defaultValue="team"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel value="user" control={<Radio />} label="User" 
+          onClick={() => {var tmpContext = context; tmpContext.per = "user"; console.log(tmpContext);setContext(tmpContext)}} />
+        <FormControlLabel value="team" control={<Radio />} label="Across team" 
+          onClick={() => {var tmpContext = context; tmpContext.per = "team"; console.log(tmpContext);setContext(tmpContext)}} />
+        <FormControlLabel value="topic" control={<Radio />} label="Topic" 
+          onClick={() => {var tmpContext = context; tmpContext.per = "topic"; console.log(tmpContext);setContext(tmpContext)}} />
+        <FormControlLabel value="reactions_sent" control={<Radio />} label="reactions sent" 
+          onClick={() => {var tmpContext = context; tmpContext.per = "reactions_sent"; console.log(tmpContext);setContext(tmpContext)}} />
+      </RadioGroup>
+    </FormControl>
   );
 }
