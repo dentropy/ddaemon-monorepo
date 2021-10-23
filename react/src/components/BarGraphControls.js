36a31f2bb3ee40f6d09a63258fb34fa7a67d9f84
Paul Mullins <paulmullins@protonmail.com>
2021-10-23 15:48:45 -0400

    Now programatically generates the graph controls

--- react/src/components/BarGraphControls.js
+++ react/src/components/BarGraphControls.js
@@ -8,6 +8,55 @@ import { Context } from '../Provider';
 import { Box } from '@mui/system';
 export default function BarGraphControls() {
   const [state, dispatch] = React.useContext(Context);
+  const [graphControls, setGraphControls] = React.useState(<h1>Loading Graph Controls</h1>)
+
+
+  React.useEffect(() => {
+    async function doAsync(){
+      let myData = await (await fetch('/query', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({
+          "index": "keybase-*",
+          "query": {
+            "query": {
+                "bool": {
+                    "must": [
+                        {
+                            "exists": {
+                                "field": "msg.content.type"
+                            }
+                        }
+                    ]
+                }
+            },
+            "aggs": {
+                "departments": {
+                    "terms": {
+                        "field": "msg.content.type"
+                    }
+                }
+            },
+          "size": 0
+          }
+        })
+      })).json()
+      let graph_controls = [];
+      graph_controls.push()
+      myData.aggregations.departments.buckets.forEach((thingy) => {
+        graph_controls.push(
+          <>
+            <FormControlLabel value={thingy.key} control={<Radio />} label={thingy.key} 
+              onClick={() => { dispatch({ type: "MOST", payload: thingy.key})}} />
+          </>
+        )
+      })
+      setGraphControls(graph_controls)
+    }
+    doAsync()
+  }, [])
   return(
     <Box>
         <FormControl component="fieldset">
@@ -17,14 +66,7 @@ export default function BarGraphControls() {
           defaultValue="messages"
           name="radio-buttons-group"
         >
-          <FormControlLabel value="messages" control={<Radio />} label="messages" 
-            onClick={() => { dispatch({ type: "MOST", payload: "text"})}} />
-          <FormControlLabel value="edits" control={<Radio />} label="edits" 
-            onClick={() => { dispatch({ type: "MOST", payload: "edit"})}}  />
-          <FormControlLabel value="deletes" control={<Radio />} label="deletes" 
-            onClick={() => { dispatch({ type: "MOST", payload: "delete"})}} />
-          <FormControlLabel value="reactions_sent" control={<Radio />} label="reactions sent"
-            onClick={() => { dispatch({ type: "MOST", payload: "reaction"})}}  />
+          {graphControls}
         </RadioGroup>
         <FormLabel component="legend">per _____</FormLabel>
         <RadioGroup
@@ -34,8 +76,6 @@ export default function BarGraphControls() {
         >
             <FormControlLabel value="user" control={<Radio />} label="User" 
               onClick={() => { dispatch({ type: "PER", payload: "msg.sender.username"})}} />
-            <FormControlLabel value="team" control={<Radio />} label="Across team" 
-              onClick={() => { dispatch({ type: "PER", payload: "msg.channel.name"})}} />
             <FormControlLabel value="topic" control={<Radio />} label="Topic" 
               onClick={() => { dispatch({ type: "PER", payload: "msg.channel.topic_name"})}} />
           </RadioGroup>
