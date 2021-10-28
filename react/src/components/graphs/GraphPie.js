d5d4728c7d11627a7cb2cb153901f5c1ebeb8aab
Paul Mullins <paulmullins@protonmail.com>
2021-10-28 15:28:16 -0400

    Got a pie chart working

--- react/src/components/graphs/GraphPie.js
+++ react/src/components/graphs/GraphPie.js
@@ -3,6 +3,15 @@ import { VegaLite } from 'react-vega'
 import { JSCharting } from 'jscharting-react';
 // [Pie Chart | Vega-Lite](https://vega.github.io/vega-lite/examples/arc_pie.html)
 export const GraphPie =  (props) => {
+    console.log("console.log(props) --- GraphPie")
+    console.log(props)
+    let point_list = []
+    props.meta_data.table.forEach((item) => {
+        point_list.push({
+            "name":item.key,
+            "y":item.doc_count
+        })
+    })
     const config = {
         debug: true, 
         title_position: 'center', 
@@ -17,24 +26,37 @@ export const GraphPie =  (props) => {
           pointSelection: true
         }, 
         defaultPoint_label_text: '<b>%name</b>', 
-        title_label_text: 'Countries GDP', 
+        title_label_text: props.title + " " + props.subtitle, 
         yAxis: { label_text: 'GDP', formatString: 'n' }, 
+        // defaultPoint: { 
+        //     label: { 
+        //       text: '%name<br/><b>%percentOfTotal%</b>', 
+        //       placement: 'outside', 
+        //       style_fontSize: 14 
+        //     }, 
+        //     marker: { 
+        //       visible: true, 
+        //       size: 30, 
+        //       fill: ['white', 0.9], 
+        //       outline_width: 0 
+        //     } 
+        //   }, 
         series: [ 
           { 
             name: 'Countries', 
-            points: [ 
-              { name: 'United States', y: 5452500 }, 
-              { name: 'Canada', y: 786052 }, 
-              { name: 'United Kingdom', y: 477338 }, 
-              { name: 'Mexico', y: 155313 } 
-            ] 
+            points: point_list,
+            // [ 
+            //   { name: 'United States', y: 5452500 }, 
+            //   { name: 'Canada', y: 786052 }, 
+            //   { name: 'United Kingdom', y: 477338 }, 
+            //   { name: 'Mexico', y: 155313 } 
+            // ] 
           } 
         ] 
     };
-    
     const divStyle = {
-        maxWidth: '700px',
-        height: '400px',
+        maxWidth: props.graph_width,//'1000px',
+        height: props.graph_height,
         margin: '0px auto'
     };
     
