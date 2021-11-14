12e4ca970ad9daba7c812d44b5665760ef84e862
Paul Mullins <paulmullins@protonmail.com>
2021-11-14 01:25:03 -0500

    Just got lines removed and lines added working

--- test.js
+++ test.js
@@ -16,31 +16,45 @@
 const { execSync } = require("child_process");
 const fs = require('fs');
 
+
+async function get_single_commit_data(team_name, repo_name, tmp_commit){
+    let commit_result = {
+      "id":team_name + "." + repo_name + "." + tmp_commit,
+      "hash": tmp_commit
+    }
+    var checkout_command = `cd tmpRepo && git log -n 1 --pretty=format:"%an" ` + tmp_commit
+    commit_result.author_name  = await execSync(checkout_command).toString()
+    checkout_command = `cd tmpRepo && git log -n 1 --pretty=format:"%ae" ` + tmp_commit
+    commit_result.author_email = await execSync(checkout_command).toString()
+    checkout_command = `cd tmpRepo && git log -n 1 --pretty=format:"%ad" ` + tmp_commit
+    commit_result.author_date = await execSync(checkout_command).toString()
+    checkout_command = `cd tmpRepo && git log -n 1 --pretty=format:"%s" ` + tmp_commit
+    commit_result.subject = await execSync(checkout_command).toString()
+    checkout_command = `cd tmpRepo && git diff-tree --no-commit-id --name-only -r ` + tmp_commit
+    commit_result.files_changed = await execSync(checkout_command).toString().split("\n")
+    commit_result.files_changed.pop()
+    return(commit_result)
+}
+
 async function get_git_repo_metadata(repo_url){
     let team_name = repo_url.split("/")[3]
     let repo_name = repo_url.split("/")[4]
     // let git_clone_command = `git clone ${repo_url}.git tmpRepo`
     // await execSync(git_clone_command)
     let commit_hashes = await execSync("cd tmpRepo && git log --pretty=oneline").toString().split("\n")
+    commit_hashes.pop()
+    commit_hashes.reverse()
     console.log(commit_hashes)
-    console.log(commit_hashes.length)
-    for(var i = 0; i < commit_hashes.length; i++){
-      commit_result = {
-        "id":team_name + "." + repo_name + "." + commit_hashes[i].split(" ")[0],
-        "hash": commit_hashes[i].split(" ")[0]
-      }
-      var checkout_command = `cd tmpRepo && git log -n 1 --pretty=format:"%an" ` + commit_hashes[i].split(" ")[0]
-      commit_result.author_name  = await execSync(checkout_command).toString()
-      checkout_command = `cd tmpRepo && git log -n 1 --pretty=format:"%ae" ` + commit_hashes[i].split(" ")[0]
-      commit_result.author_email = await execSync(checkout_command).toString()
-      checkout_command = `cd tmpRepo && git log -n 1 --pretty=format:"%ad" ` + commit_hashes[i].split(" ")[0]
-      commit_result.author_date = await execSync(checkout_command).toString()
-      checkout_command = `cd tmpRepo && git log -n 1 --pretty=format:"%s" ` + commit_hashes[i].split(" ")[0]
-      commit_result.subject = await execSync(checkout_command).toString()
-      checkout_command = `cd tmpRepo && git diff-tree --no-commit-id --name-only -r ` + commit_hashes[i].split(" ")[0]
-      commit_result.files_changed = await execSync(checkout_command).toString().split("\n")
-      commit_result.files_changed.pop()
-      console.log(commit_result)
+
+    for(var i = 0; i < commit_hashes.length - 1; i++){
+        let commit_metadata = await get_single_commit_data(team_name, repo_name, commit_hashes[i].split(" ")[0])
+        var checkout_command = `cd tmpRepo && git diff --stat ${commit_hashes[i].split(" ")[0]} ${commit_hashes[i + 1].split(" ")[0]}`
+        console.log(checkout_command)
+        commit_metadata.lines_changed_raw  = await execSync(checkout_command).toString()
+
+        commit_metadata.lines_added   = commit_metadata.lines_changed_raw.split("+").length - 1
+        commit_metadata.lines_removed = commit_metadata.lines_changed_raw.split("-").length - 1
+        console.log(commit_metadata)
     }
     // console.log(team_name)
     // console.log(repo_name)
