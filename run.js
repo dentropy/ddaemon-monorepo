a7281a72d0e380066b85d49cea0332a20b3bdcb4
Paul Mullins <paulmullins@protonmail.com>
2021-11-14 02:01:03 -0500

    That was super annoying

--- run.js
+++ run.js
@@ -9,5 +9,27 @@ async function doAsync(){
 //doAsync()
 
 const fs = require('fs');
-let rawdata = JSON.parse(fs.readFileSync('test.json'));
-console.log(rawdata);
+const urlRegex = require('url-regex');
+let rawdata = fs.readFileSync('coins_enriched.json').toString()
+rawdata.replace("\\/\\/", "\\");
+
+
+let tmp_urls = rawdata.match(urlRegex())
+new_urls = []
+tmp_urls.forEach((my_url) => {
+  if(my_url.indexOf("github") > -1)
+  {
+    console.log(my_url)
+    new_urls.push(my_url)
+  }
+})
+console.log(new_urls)
+fs.writeFileSync("gitrepos.json", JSON.stringify(new_urls))
+// let http_regex = "^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$"
+// var myRe = new RegExp(http_regex);
+// var myArray = myRe.exec(rawdata);
+// console.log(myArray)
+// console.log ( rawdata.split("github").length )
+
+// let test = "https:\/\/www.tokocrypto.com\/trade\/SCUSDT"
+// console.log(test.replace("\\/\\/", "\\"))
