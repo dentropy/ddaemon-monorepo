a73ca7054f296d01b4e40866fe9bd2020653f0e8
Paul Mullins <paulmullins@protonmail.com>
2021-11-14 01:34:37 -0500

    Export files works

--- test.js
+++ test.js
@@ -17,6 +17,11 @@ const { execSync } = require("child_process");
 const fs = require('fs');
 
 
+let export_dir = "./exports"
+if (!fs.existsSync(export_dir)){
+    fs.mkdirSync(export_dir, { recursive: true });
+}
+
 async function get_single_commit_data(team_name, repo_name, tmp_commit){
     let commit_result = {
       "id":team_name + "." + repo_name + "." + tmp_commit,
@@ -32,6 +37,7 @@ async function get_single_commit_data(team_name, repo_name, tmp_commit){
     commit_result.subject = await execSync(checkout_command).toString()
     checkout_command = `cd tmpRepo && git diff-tree --no-commit-id --name-only -r ` + tmp_commit
     commit_result.files_changed = await execSync(checkout_command).toString().split("\n")
+    commit_result.num_files_changed = commit_result.files_changed.length - 1
     commit_result.files_changed.pop()
     return(commit_result)
 }
@@ -39,25 +45,22 @@ async function get_single_commit_data(team_name, repo_name, tmp_commit){
 async function get_git_repo_metadata(repo_url){
     let team_name = repo_url.split("/")[3]
     let repo_name = repo_url.split("/")[4]
-    // let git_clone_command = `git clone ${repo_url}.git tmpRepo`
-    // await execSync(git_clone_command)
     let commit_hashes = await execSync("cd tmpRepo && git log --pretty=oneline").toString().split("\n")
     commit_hashes.pop()
     commit_hashes.reverse()
     console.log(commit_hashes)
-
-    for(var i = 1; i < commit_hashes.length - 1; i++){
+    let repo_metadata = []
+    for(var i = 1; i < commit_hashes.length - 1; i++) {
         let commit_metadata = await get_single_commit_data(team_name, repo_name, commit_hashes[i].split(" ")[0])
         var checkout_command = `cd tmpRepo && git diff --stat ${commit_hashes[i - 1].split(" ")[0]} ${commit_hashes[i].split(" ")[0]}`
-        console.log(checkout_command)
         commit_metadata.lines_changed_raw  = await execSync(checkout_command).toString()
-
         commit_metadata.lines_added   = commit_metadata.lines_changed_raw.split("+").length - 1
         commit_metadata.lines_removed = commit_metadata.lines_changed_raw.split("-").length - 1
+        console.log(checkout_command)
         console.log(commit_metadata)
+        repo_metadata.push(commit_metadata)
     }
-    // console.log(team_name)
-    // console.log(repo_name)
+    fs.writeFileSync(`${export_dir}/${team_name}.${repo_name}.json`, JSON.stringify(repo_metadata));
 }
 
 
